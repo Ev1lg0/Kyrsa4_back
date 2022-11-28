f8f4c070eb59a3a0fb80c2fd287b433364465cc2
Local <56597787+l2700l@users.noreply.github.com>
2022-11-28 18:32:27 +0400

    Update modelsController.js

--- dist/api/modelsController.js
+++ dist/api/modelsController.js
@@ -3,10 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.models = void 0;
+exports.models = exports.sequelize = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 const sequelize_1 = require("sequelize");
 const init_models_1 = require("./models/init-models");
 dotenv_1.default.config();
-const sequelize = new sequelize_1.Sequelize(`${process.env.CONNECTION_STRING}`);
-exports.models = (0, init_models_1.initModels)(sequelize);
+exports.sequelize = new sequelize_1.Sequelize(`${process.env.CONNECTION_STRING}`);
+exports.models = (0, init_models_1.initModels)(exports.sequelize);
